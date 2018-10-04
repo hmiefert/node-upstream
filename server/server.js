d2c2bc392ff0212818d914d5e2d2a853c5b7cb02
Holger Miefert <holger.miefert@krzn.de>
2018-10-04 11:37:55 +0200

    Server: Added debug output

--- server/server.js
+++ server/server.js
@@ -4,21 +4,30 @@ const path = require('path');
 
 const app = express();
 
+app.set('port', process.env.PORT || 3000);
+app.set('host', process.env.HOST || 'localhost');
+
 app.post('/upload', (req, res) => {
-    const targetFilePath = path.join(__dirname, 'uploads', 'upload.mp4')
+    console.log('Receiving upload request with headers: ' + JSON.stringify(req.headers, null, 2));
+
+    const targetFilePath = path.join(__dirname, 'uploads', 'upload.mp4');
     const targetFileStream = fs.createWriteStream(targetFilePath);
-    
+    let chunkCounter = 0
+    let uploadSize = 0
+
     req.on('data', (chunk) => {
-        targetFileStream.write(chunk);    
+        console.log(`Received chunk #${chunkCounter++} with ${chunk.length.toString()} bytes.`)
+        targetFileStream.write(chunk);
+        uploadSize += chunk.length
     });
 
     req.on('end', () => {
-        console.log('Upload finished')
-        targetFileStream.close()
-        res.json({message: 'Upload successful..'});
+        console.log(`Upload finished. Received a total of ${uploadSize} bytes.`);
+        targetFileStream.close();
+        res.json({message: 'Upload finished'});
     });
-})
+});
 
-app.listen(3000, () => {
-    console.log('Server running at http://localhost:3000');
-})
+app.listen(app.get('port'), app.get('host'), () => {
+    console.log(`Server running at http://${app.get('host')}:${app.get('port')}`);
+});
